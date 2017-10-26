f740ccf5c83362b0a8e6d536d9bdb89647b6c6a8
shyam <shyamp99@gmail.com>
2017-10-26 13:10:22 -0400

    first commit

--- src/LuckyNines.java
+++ src/LuckyNines.java
@@ -1,4 +1,24 @@
-package PACKAGE_NAME;
-
 public class LuckyNines {
+    public static void main(String[] args) {
+        int a = IO.readInt();
+        int b = IO.readInt();
+        System.out.println(countLuckyNines(a,b));
+    }
+    public static int countLuckyNines( int lowerEnd, int upperEnd){
+        int count = 0;
+        int starter = 0;
+        if (upperEnd<lowerEnd||upperEnd<0&&lowerEnd>0)
+            return -1;
+        for (int i = lowerEnd; i<=upperEnd+1; i++){
+            if (i == upperEnd+1)
+                return -1;
+            if (i%10==9){
+                starter = i;
+                break;
+            }
+        }
+        for(int i = starter; i<upperEnd; i+=10)
+            count++;
+        return count;
+    }
 }
