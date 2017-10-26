5332ac300242e4016b4b2930a59d499a23705764
shyam <shyamp99@gmail.com>
2017-10-26 15:48:00 -0400

    fixed luckies

--- src/LuckyNines.java
+++ src/LuckyNines.java
@@ -1,24 +1,16 @@
 public class LuckyNines {
     public static void main(String[] args) {
-        int a = IO.readInt();
-        int b = IO.readInt();
-        System.out.println(countLuckyNines(a,b));
-    }
-    public static int countLuckyNines( int lowerEnd, int upperEnd){
+        int lowerEnd = IO.readInt();
+        int upperEnd = IO.readInt();
         int count = 0;
-        int starter = 0;
-        if (upperEnd<lowerEnd||upperEnd<0&&lowerEnd>0)
-            return -1;
-        for (int i = lowerEnd; i<=upperEnd+1; i++){
-            if (i == upperEnd+1)
-                return -1;
-            if (i%10==9){
-                starter = i;
-                break;
-            }
+        if (lowerEnd > upperEnd)
+            IO.reportBadInput();
+        for (int i = lowerEnd; i < upperEnd + 1; i++) {
+            String s = Integer.toString(i);
+            if (s.indexOf("9") != -1)
+                count++;
+
         }
-        for(int i = starter; i<upperEnd; i+=10)
-            count++;
-        return count;
+        IO.outputIntAnswer(count);
     }
 }
