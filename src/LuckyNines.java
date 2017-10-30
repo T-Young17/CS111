75d4987f539e138c070ef7916da0f2cf79ab5934
shyam <shyamp99@gmail.com>
2017-10-30 17:18:01 -0400

    77 and 99 counts as 2 fixed

--- src/LuckyNines.java
+++ src/LuckyNines.java
@@ -1,22 +1,35 @@
 public class LuckyNines {
     public static void main(String[] args) {
-        int lowerEnd = IO.readInt();
-        int upperEnd = IO.readInt();
-        IO.outputIntAnswer(countLuckyNines(lowerEnd, upperEnd));
+            System.out.print("Enter lower bound: " );
+            int lower = IO.readInt();
 
-}
-    public static int countLuckyNines(int lowerEnd, int upperEnd){
-        int count = 0;
-        if (lowerEnd > upperEnd || upperEnd > lowerEnd){
-            IO.reportBadInput();
-            return -1;}
-        for (int i = lowerEnd; i < upperEnd + 1; i++) {
-            String s = Integer.toString(i);
-            if (s.indexOf("9") != -1)
-                count++;
+            System.out.print("Enter higher bound: ");
+            int higher = IO.readInt();
 
+            IO.outputDoubleAnswer(countLuckyNines(lower,higher));
         }
-        return count;
 
+
+    public static int countLuckyNines(int lowerEnd, int upperEnd) {
+        int count = 0;
+        if(lowerEnd > upperEnd || upperEnd < lowerEnd) {
+            IO.reportBadInput();
+            return -1;
+        }
+        for(int i = lowerEnd; i <= upperEnd; i ++) {
+            int term = Math.abs(i);
+
+
+            while(term > 0) {
+                if(term % 10 == 9) {
+                    count ++;
+                }
+
+                term /= 10;
+            }
+        }
+
+        return count;
     }
+
 }
