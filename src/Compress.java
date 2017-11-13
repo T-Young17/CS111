8780d1faf72ffb617377369e6f6b1a394a661bf4
shyam <shyamp99@gmail.com>
2017-11-13 14:44:34 -0500

    HW 4 finished

--- src/Compress.java
+++ src/Compress.java
@@ -1,18 +1,19 @@
 public class Compress {
     public static void main(String[] args) {
-    String s = "abcdasdasdsaddadasdefs";
+        String s = "aaaaeeedddaAAAAvvv   l";
         System.out.println(compress(s));
 
     }
-    public static String compress (String s){
-        //should ignore numbers and punctuation of any sort
-        String temp="";
+
+    public static String compress(String s) {
+        //punctation is treated the same as letters, assuming no numbers are in inputs
+        String temp = "";
         int count = 0;
-        String l= "";
-        if(s.equals(null)||s.equals(""))
+        String l = "";
+        if (s.equals(null) || s.equals(""))
             return null;
 
-        for (int i =0; i < s.length(); i++) {
+        for (int i = 0; i < s.length(); i++) {
 
             if (i == 0 || l.charAt(0) != s.charAt(i)) {
                 if (count > 1)
@@ -21,15 +22,22 @@ public class Compress {
                     temp += l;
                 l = s.substring(i, i + 1);
                 count = 0;
-            } else if (i == 0)
+            }
+
+            else if (i == 0)
                 l = s.substring(0, 1);
 
             if (s.substring(i, i + 1).equals(l))
                 count++;
-            if (i == s.length() - 1)
+
+            if (i == s.length() - 1 && count > 1)
                 temp += Integer.toString(count) + l;
+            else if (i == s.length()-1 && count == 1)
+                temp+= l;
         }
-            return temp;
-    }}
+
+        return temp;
+    }
+}
 
 
--- src/Tester.java
+++ src/Tester.java
@@ -1,6 +1,6 @@
 public class Tester {
     public static void main(String[] args) {
-        String s = "This sentence should output 5 words";
+        String s = "This sentence should output 5 words.";
         String [] a = s.split(" ");
         System.out.println(a.length);
         int i = 0;
