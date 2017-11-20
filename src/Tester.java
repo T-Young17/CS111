aa16a6373dd49a4417adc7c25ed754a32c6d6f72
shyam <shyamp99@gmail.com>
2017-11-20 18:28:16 -0500

    poker project

--- src/Tester.java
+++ src/Tester.java
@@ -1,13 +1,11 @@
 public class Tester {
     public static void main(String[] args) {
         String s = "This sentence should output 5 words.";
-        String [] a = s.split(" ");
-        System.out.println(a.length);
+        //String [] a = s.split(" ");
+        //System.out.println(a.length);
         int i = 0;
-        while(i < a.length){
-            System.out.println(a[i]+"| e");
-            i++;
-        }
+        double a = 100;
+        System.out.println(9+"3");
     }
 }
 
